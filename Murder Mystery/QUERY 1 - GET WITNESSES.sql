cf931e91877e43f658c0b710b9a8a5d7193cf4c8
Maulik Jagani <66970927+MvJ96@users.noreply.github.com>
2022-12-13 13:52:15 +0530

    Delete Murder Mystery directory

--- Murder Mystery/QUERY 1 - GET WITNESSES.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM crime_scene_report csr
-WHERE csr.date = 20180115 AND type = 'murder' AND city = 'SQL City'
-
-/*  RESULT
-    Security footage shows that there were 2 witnesses. 
-    The first witness lives at the last house on "Northwestern Dr". 
-    The second witness, named Annabel, lives somewhere on "Franklin Ave".
-*/
